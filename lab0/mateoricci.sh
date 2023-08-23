42fc544143dcabaebf805740b714633eb3503dd5
Mateo Ricci Villarruel <mateo.ricci.v@gmail.com>
2023-08-22 22:32:44 -0300

    retoques sobre el lab 0

--- lab0/mateoricci.sh
+++ lab0/mateoricci.sh
@@ -1,27 +1,113 @@
 #!/bin/bash
 
-grep "model name" /proc/cpuinfo #1
+echo ""
+echo "--------------"
+echo "Ejercicio 1"
+echo ""
 
-grep "model name" /proc/cpuinfo  | wc -l #2
+{   #try
+    grep "model name" /proc/cpuinfo 
+} || {  #catch
+    echo "Error en el ejercicio 1"
+}
 
-wget https://www.gutenberg.org/files/11/11-0.txt && sed -i 's/Alice/Mateo/g' 11-0.txt && mv 11-0.txt mateo_in_wonderwall.txt #3 descarga - modifica - renombra
+echo ""
+echo "--------------"
+echo "Ejercicio 2"
+echo ""
 
-sort -n -k 5 weather_cordoba.in | awk 'NR==1{print $1, $2, $3}' && sort -nr -k 5 weather_cordoba.in | awk 'NR==1{print $1, $2, $3}' #4, maximos y minimos sobre el maximo
+{   #try
+    grep "model name" /proc/cpuinfo  | wc -l
+} || {  #catch
+    echo "Error en el ejercicio 2"
+}
 
-sort -n -k 3 atpplayers.in #5
+echo ""
+echo "--------------"
+echo "Ejercicio 3"
+echo ""
 
-awk '{diff = $7 - $8;print $0, diff;}' "superliga.in" | sort -k2nr -k9nr #6, se agrega una nueva columna asi mostramos la diferencia tambien
+{   #try
+    wget https://www.gutenberg.org/files/11/11-0.txt && sed -i 's/Alice/Mateo/g' 11-0.txt && mv 11-0.txt mateo_in_wonderwall.txt #descarga - modifica - renombra
+} || {  #catch
+    echo "Error en el ejercicio 3"
+}
 
-ip link show | awk '/ether/ {print "MAC address:", $2}' #7
+echo ""
+echo "--------------"
+echo "Ejercicio 4"
+echo ""
 
-#8
-mkdir suits && cd suits && for i in {01..10}; do touch suits_S01E"$i"_es.srt; done
-for file in *; do mv "$file" "$(echo "$file" | sed 's/_es//g')"; done
+{   #try
+    sort -n -k 5 weather_cordoba.in | awk 'NR==1{print $1, $2, $3}' && sort -nr -k 5 weather_cordoba.in | awk 'NR==1{print $1, $2, $3}' #maximos y minimos sobre el maximo
+} || {  #catch
+    echo "Error en el ejercicio 4"
+}
 
-#opcional
+echo ""
+echo "--------------"
+echo "Ejercicio 5"
+echo ""
 
-ffmpeg -i exercise_9_a.49 -ss 00:00:04 -to 00:00:20 exercise_9_a_solved.mp4 #a
+{   #try
+    sort -n -k 3 atpplayers.inm
+} || {  #catch
+    echo "Error en el ejercicio 5"
+}
 
-ffmpeg -i vocals_ok.mp3 -i base_ok.mp3 -filter_complex amix=inputs=2:duration=longest exercise_9_b.mp3 #b
+echo ""
+echo "--------------"
+echo "Ejercicio 6"
+echo ""
+
+{   #try
+    awk '{diff = $7 - $8;print $0, diff;}' "superliga.in" | sort -k2nr -k9nr #se agrega una nueva columna asi mostramos la diferencia tambien
+} || {  #catch
+    echo "Error en el ejercicio 6"
+}
+
+echo ""
+echo "--------------"
+echo "Ejercicio 7"
+echo ""
+
+{   #try
+    ip link show | awk '/ether/ {print "MAC address:", $2}'
+} || {  #catch
+    echo "Error en el ejercicio 7"
+}
+
+echo ""
+echo "--------------"
+echo "Ejercicio 8"
+echo ""
+
+{   #try
+    mkdir suits && cd suits && for i in {01..10}; do touch suits_S01E"$i"_es.srt; done
+} || {  #catch
+    echo "Error en el ejercicio 8-a"
+}
+
+{   #try
+    for file in *; do mv "$file" "$(echo "$file" | sed 's/_es//g')"; done
+} || {  #catch
+    echo "Error en el ejercicio 8-b"
+}
 
 
+echo ""
+echo "--------------"
+echo "Ejercicio opcional"
+echo ""
+
+{   #try
+    ffmpeg -i exercise_9_a.49 -ss 00:00:04 -to 00:00:20 exercise_9_a_solved.mp4
+} || {  #catch
+    echo "Error en el ejercicio opcional-a"
+}
+
+{   #try
+    ffmpeg -i vocals_ok.mp3 -i base_ok.mp3 -filter_complex amix=inputs=2:duration=longest exercise_9_b.mp3
+} || {  #catch
+    echo "Error en el ejercicio opcional-b"
+}
